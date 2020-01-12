c3fed98abb19345c74dde8cafe6ba493e13e685f
David Cornelis Groothuizen Dijkema <d.groothuizen.dijkema@gmail.com>
2020-01-12 15:38:12 +1300

    Refactored parsing of number

--- src/main.rs
+++ src/main.rs
@@ -16,40 +16,33 @@ use std::io::Write;
 mod graph;
 mod squaresum;
 
-use graph::Graph;
 use squaresum::{square_sum_graph,square_sum_graph_append};
 
 fn main()
 {
   let args: Vec<String>=std::env::args().collect();
-  let mut n:u32;
+  let mut input: String;
   if args.len()==1
   {
     // get a number from the user
     print!("Enter n: ");
     std::io::stdout().flush();
-    let mut guess=String::new();
-    std::io::stdin().read_line(&mut guess)
+    input=String::new();
+    std::io::stdin().read_line(&mut input)
       .expect("Failed to read line.");
-    n=match guess.trim().parse()
-    {
-      Ok(num) => num,
-      Err(_) => std::process::exit(0),
-    };
   }
-  else
+  else { input=args[1].clone(); }
+  let n: u32=match input.trim().parse()
   {
-    n=match args[1].trim().parse()
-    {
-      Ok(num) => num,
-      Err(_) => std::process::exit(0),
-    };
-  }
+    Ok(num) => num,
+    Err(_) => std::process::exit(0),
+  };
+  drop(input);
   
 
   // produce the graph and find the hamiltonian path, with timing
   let now: std::time::Instant=std::time::Instant::now();
-  let gr: Graph<u32>=square_sum_graph(n);
+  let gr: graph::Graph<u32>=square_sum_graph(n);
   println!("{}",gr.hamiltonian_path().unwrap_or(graph::Path::new()));
   println!("Elapsed time: {}ms",now.elapsed().as_millis());
   println!("{}",gr);
